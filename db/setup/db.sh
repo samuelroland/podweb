03641d98f8b789bf31ee9e7aa443ed407cd132d7
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-12-15 00:48:23 +0100

    Finish db.sh

--- db/setup/db.sh
+++ db/setup/db.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+echo "Podweb database setup script"
+
+# Choose your user that can create the db podweb
 export PGPASSWORD=postgres
-files=(podweb-schema.sql podweb-additions.sql podweb-podcasts-population.sql podweb-episodes-population.sql)
-set -e
+export PGUSER=postgres
+
+files=(podweb-schema.sql podweb-data.sql podweb-additions.sql)
+
 for file in "${files[@]}"; do
-    echo "Launching $file:"
-	psql -h "127.0.0.1" -U postgres -a -f $file
+    echo "
+--- Running $file..."
+	psql -h "127.0.0.1" -U "$PGUSER" -a -f $file -v ON_ERROR_STOP=1 | grep ERROR
 done
 
-echo "Seems to be all good !"
+echo "
+Seems to be all good !"
