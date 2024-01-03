ef701c195474d1128eb0c684d28712442c7afdf4
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-01-04 00:29:57 +0100

    Find a hacky way to integrate TailwindCSS with the gradle plugin "au.id.wale.tailwind"

--- podweb/app/src/main/static/tailwind.config.js
+++ podweb/app/src/main/static/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.jte"],
+	content: ["**/jte/*.jte"],
 	theme: {
 		extend: {},
 	},
---
+++ podweb/tailwind-watch.sh
@@ -0,0 +1,2 @@
+# Hacky command to run the tailwind CLI downloaded by the Gradle plugin "au.id.wale.tailwind" to avoid needing the installation of NPM just to use this.
+~/.gradle/caches/au.id.wale.tailwind/3.4.0/tailwindcss-linux-x64 -i ./app/src/main/static/style.css -o ./app/src/main/static/out.css --watch=always
