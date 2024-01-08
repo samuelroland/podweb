1096fbb6e0257deb615750a9db3263b2b9eecee5
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-01-08 19:37:32 +0100

    Remove any tailwind build code in Gradle builds

--- podweb/tailwind-watch.sh
+++
@@ -1,6 +0,0 @@
-# Hacky command to run the tailwind CLI downloaded by the Gradle plugin "au.id.wale.tailwind" to avoid needing the installation of NPM just to use this.
-~/.gradle/caches/au.id.wale.tailwind/3.4.0/tailwindcss-linux-x64 \
-	-i ./app/src/main/static/style.css \
-	-o ./app/src/main/static/out.css \
-	-c ./app/src/main/static/tailwind.config.js\
-	--watch=always -v
