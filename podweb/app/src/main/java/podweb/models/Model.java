d737686a5cd0763963b33d5a60b1006b3ce0e7f0
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-01-15 23:23:41 +0100

    Big progress in Model with getBy(), getFirstBy(), count(), exists(), create(), delete(), update() WIP

--- podweb/app/src/main/java/podweb/models/Model.java
+++ podweb/app/src/main/java/podweb/models/Model.java
@@ -1,8 +1,11 @@
 package podweb.models;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public abstract class Model<T> {
+    public int id;
+
     abstract public String table();
 
     public static Podcast o = new Podcast();
@@ -20,7 +23,7 @@ public abstract class Model<T> {
         return getQuery().query(query, new Object[] { id });
     }
 
-    public ArrayList<T> getBy(String[] foreignKeys, int[] ids) {
+    public ArrayList<T> getBy(String[] foreignKeys, Integer[] ids) {
         String query = "select * from " + table()
                 + " where ";
 
@@ -33,6 +36,40 @@ public abstract class Model<T> {
         return getQuery().query(query, ids);
     }
 
+    public T getFirstBy(String[] foreignKeys, Integer[] ids) {
+        ArrayList<T> list = getBy(foreignKeys, ids);
+        if (!list.isEmpty()) {
+            return list.getFirst();
+        }
+        return null;
+    }
+
+    public T getFirstBy(String foreignKey, Object o) {
+        ArrayList<T> list = getBy(foreignKey, new Object[] { o });
+        if (!list.isEmpty()) {
+            return list.getFirst();
+        }
+        return null;
+    }
+
+    public ArrayList<T> getBy(String foreignKey, Object o) {
+        return getBy(foreignKey, new Object[] { o });
+    }
+
+    public ArrayList<T> getBy(String foreignKey, Object[] o) {
+        String query = "select * from " + table()
+                + " where " + foreignKey + " = ? ;";
+        return getQuery().query(query, o);
+    }
+
+    public T getFirstBy(String foreignKey, Object[] o) {
+        ArrayList<T> list = getBy(foreignKey, o);
+        if (!list.isEmpty()) {
+            return list.getFirst();
+        }
+        return null;
+    }
+
     public T find(int id) {
         ArrayList<T> list = getQuery().query("select * from " + table()
                 + " where id = ?", new Object[] { id });
@@ -43,24 +80,65 @@ public abstract class Model<T> {
     }
 
     public boolean exists(int id) {
-        return getQuery().query("select top 1 from " + table() +
-                " where id = ?", new Object[] { id }) == null;
+        // TODO: check sql exists performant way
+        ArrayList<T> list = getQuery().query("select 1 from " + table() +
+                " where id = ?", new Object[] { id });
+        return (list != null && list.size() == 1);
     }
 
-    // public T create() {
-    // // TODO
-    // String foreignKeysList = "";
-    // String integerrogationMarks = "";
-    // for (int i = 0; i < foreignKeys.length; i++) {
-    // if (i > 0) {
-    // foreignKeysList += ", ";
-    // integerrogationMarks += ", ";
-    // }
-    // foreignKeysList += foreignKeys[i];
-    // integerrogationMarks += "?";
-    // }
-    // query += "" +
-    // }
+    public int count() {
+        return Query.count(table());
+    }
+
+    public boolean create() {
+        String query = "insert into " + table() + " (";
+
+        String attributes = "";
+        String interogationMarks = "";
+        int i = 0;
+        for (Field field : getQuery().ref.getFields()) {
+            if ((field.getName().equals("id") || field.getName().equals("o") || field.getName().equals("q")))
+                continue;
+            if (i > 0) {
+                attributes += ", ";
+                interogationMarks += ", ";
+            }
+            attributes += field.getName();
+            interogationMarks += "?";
+            i++;
+        }
+        query += attributes + ") values (" + interogationMarks + ");";
+        System.out.println(query);
+        int nb = getQuery().update(query, this);
+        if (nb > 0) {
+            this.id = nb;
+        } else if (nb == 0) {
+            return false; // not an id but 0 line affected so action failed
+        }
+        return true;
+    }
+
+    public boolean update() {
+        String query = "UPDATE " + table() + "\nSET ";
+
+        String attributes = "";
+        int i = 0;
+        for (Field field : getQuery().ref.getFields()) {
+            if ((field.getName().equals("id") || field.getName().equals("o") || field.getName().equals("q")))
+                continue;
+
+            if (i > 0) {
+                attributes += ", ";
+            }
+            attributes += field.getName() + " = ?";
+            i++;
+        }
+        query += attributes + "\nWHERE id = ?";
+        // TODO: specify ID !!!
+        System.out.println(query);
+        int nb = getQuery().update(query, this);
+        return nb == 1;
+    }
 
     public boolean delete(int id) {
         return getQuery().update("delete from " + table()
